c5dd7e4826710ab555b4b34f8f64704dd05f1860
Sounak Banerjee <sounakban@users.noreply.github.com>
2020-03-31 01:49:32 -0400

    Install vscode through snap

--- install_script.sh
+++ install_script.sh
@@ -123,11 +123,12 @@ then
 fi
 if [ $vscode_response == "y" ] 
 then
-	sudo apt install software-properties-common apt-transport-https wget -y
-	wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-	sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-	sudo apt update -y
-	sudo apt install code -y
+	sudo snap install --classic code
+	#sudo apt install software-properties-common apt-transport-https wget -y
+	#wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+	#sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+	#sudo apt update -y
+	#sudo apt install code -y
 fi
 
 sudo apt install upgrade -y
